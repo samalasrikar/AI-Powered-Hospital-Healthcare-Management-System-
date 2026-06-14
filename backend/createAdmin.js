e8a25e3f4cecf99278cb762966059ea99a8d97cf
samala srikar <148432583+samalasrikar@users.noreply.github.com>
2026-06-14 19:21:56 +0530

    Remove credentials from MongoDB connection string
    Updated MongoDB connection string to remove credentials.

--- backend/createAdmin.js
+++ backend/createAdmin.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const User = require('./src/models/User');
 
-mongoose.connect('mongodb+srv://HMS_admin:OYqLa1u5Os98TwgG@hospital-cluster.bmagxu1.mongodb.net/');
+mongoose.connect('mongodb+srv://@hospital-cluster.bmagxu1.mongodb.net/');
 
 async function createAdmin() {
   const admin = await User.create({
