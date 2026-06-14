51fe2b8c3d343a5e0b920a021b01d9fdcc0e7252
samala srikar <148432583+samalasrikar@users.noreply.github.com>
2026-06-14 20:26:20 +0530

    Implement backend security best practices

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,71 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+
+
 const routes = require('./routes');
 const { notFound, errorHandler } = require('./middlewares/errorHandler');
 
 const app = express();
 
+app.disable('x-powered-by');
+/*
+|--------------------------------------------------------------------------
+| Security Headers
+|--------------------------------------------------------------------------
+*/
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
+
+/*
+|--------------------------------------------------------------------------
+| CORS
+|--------------------------------------------------------------------------
+*/
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:5173',
+    ],
+    credentials: true,
+  })
+);
+
+/*
+|--------------------------------------------------------------------------
+| Prevent NoSQL Injection
+|--------------------------------------------------------------------------
+*/
+
+
+/*
+|--------------------------------------------------------------------------
+| Body Parser
+|--------------------------------------------------------------------------
+*/
 app.use(express.json());
+
+/*
+|--------------------------------------------------------------------------
+| Global Rate Limiter
+|--------------------------------------------------------------------------
+*/
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: {
+    success: false,
+    message: 'Too many requests. Try again later.',
+  },
+});
+
+app.use('/api', apiLimiter);
+
 app.use('/api', routes);
 
 app.use(notFound);
